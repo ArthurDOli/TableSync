fbfbc909659f1a4b06b2cdfdf1af30b62b87b1d7
Arthur <arthur.duarte.oli06@gmail.com>
2026-01-25 15:49:14 -0300

    feat: PlayerCharacterRepository added

---
+++ src/main/java/com/tablesync/tablesync/repository/PlayerCharacterRepository.java
@@ -0,0 +1,15 @@
+package com.tablesync.tablesync.repository;
+
+import com.tablesync.tablesync.entity.PlayerCharacter;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface PlayerCharacterRepository extends JpaRepository<PlayerCharacter, UUID> {
+    @EntityGraph(attributePaths = {"session", "user"})
+    List<PlayerCharacter> findBySessionId(UUID sessionId);
+}
