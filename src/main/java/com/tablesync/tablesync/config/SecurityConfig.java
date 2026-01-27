8f27184b4371a8171afc63a3a44caadc57a779e7
Arthur <arthur.duarte.oli06@gmail.com>
2026-01-27 17:55:51 -0300

    fix(SecurityConfig): swagger now works

--- src/main/java/com/tablesync/tablesync/config/SecurityConfig.java
+++ src/main/java/com/tablesync/tablesync/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/api/v1/auth/**",
-                                "/v3/api-docs/**",
+                                "/api-docs/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html"
                         ).permitAll()
