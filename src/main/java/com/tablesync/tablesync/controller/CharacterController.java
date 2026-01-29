c9901d2f8357abc638118700180606d7621ba5e9
Arthur <arthur.duarte.oli06@gmail.com>
2026-01-29 13:49:37 -0300

    fix(CharacterController): correct annotations implemented in the 'createCharacter' endpoint, avoiding error 400

--- src/main/java/com/tablesync/tablesync/controller/CharacterController.java
+++ src/main/java/com/tablesync/tablesync/controller/CharacterController.java
@@ -3,6 +3,7 @@ package com.tablesync.tablesync.controller;
 import com.tablesync.tablesync.dto.character.request.CharacterRequest;
 import com.tablesync.tablesync.dto.character.response.CharacterResponse;
 import com.tablesync.tablesync.service.CharacterService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +19,7 @@ public class CharacterController {
     private final CharacterService characterService;
 
     @PostMapping
-    public ResponseEntity<CharacterResponse> createCharacter(CharacterRequest request) {
+    public ResponseEntity<CharacterResponse> createCharacter(@RequestBody @Valid CharacterRequest request) {
         CharacterResponse character = characterService.createCharacter(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(character);
     }
