a8f7b8d91723702c8a5d89f01a86764afccecb35
Arthur <arthur.duarte.oli06@gmail.com>
2026-01-29 09:32:16 -0300

    feat: CharacterRequest DTO added

---
+++ src/main/java/com/tablesync/tablesync/dto/character/request/CharacterRequest.java
@@ -0,0 +1,32 @@
+package com.tablesync.tablesync.dto.character.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CharacterRequest {
+    @NotNull(message = "Session ID is required")
+    private UUID sessionId;
+
+    private UUID templateId;
+
+    @NotBlank(message = "Character name is required")
+    @Size(min = 2, max = 50)
+    private String name;
+
+    private String imageUrl;
+
+    @NotNull(message = "Sheet data is required")
+    private Map<String, Object> sheetData;
+}
