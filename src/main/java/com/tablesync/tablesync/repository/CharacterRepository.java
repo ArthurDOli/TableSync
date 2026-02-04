2970b148fb6c00ed878cc35e3287995fefde8d28
Arthur <arthur.duarte.oli06@gmail.com>
2026-02-04 09:28:20 -0300

    fix: JpaRepository added in CharacterRepository and TemplateRepository

--- src/main/java/com/tablesync/tablesync/repository/CharacterRepository.java
+++ src/main/java/com/tablesync/tablesync/repository/CharacterRepository.java
@@ -1,10 +1,15 @@
 package com.tablesync.tablesync.repository;
 
+import com.tablesync.tablesync.entity.PlayerCharacter;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface CharacterRepository {
+public interface CharacterRepository extends JpaRepository<PlayerCharacter, UUID> {
     boolean existsByUserIdAndSessionId(Long userId, UUID sessionId);
+    List<PlayerCharacter> findBySessionId(UUID sessionId);
+    List<PlayerCharacter> findByUserIdAndSessionId(Long userId, UUID sessionId);
 }
